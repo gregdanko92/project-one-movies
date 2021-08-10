72646f61d33520db57070bf57358bdfe77c6557e
cotsao <codytsao@gmail.com>
2021-08-10 12:44:00 -0700

    added index.ejs, navbar partial and linked static js and css pages

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const rowdyResults = rowdy.begin(app)
 app.use(methodOverride('_method')); // allows usage of non-post/get methods
 app.use(express.urlencoded({ extended: false }))  //allows req.body to draw form data
 app.use('/movies', moviesController); // set the url controller will use
+app.use(express.static(__dirname + '/public'));
 /* ------------------------------ Configuration ----------------------------- */
 app.set('view engine', 'ejs'); // changes express default view engine to ejs
 /* ------------------------------- Set Routes ------------------------------- */
